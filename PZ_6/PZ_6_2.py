419b4874a1fd64bd41d99c5cc626984c19827552
rey <gridina.08@bk.ru>
2025-11-26 16:01:28 +0300

    6 практическая

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,13 +1,18 @@
 #Дан список размера N. Найти номер его первого локального минимума (локальный
 # минимум — это элемент, который меньше любого из своих соседей).
-n = int(input("N: "))
+import random
+
+n = random.randint(3, 15)
 lst = []
 for i in range(n):
-    lst.append(int(input()))
+    lst.append(random.randint(1, 100))
+
+print("N =", n)  # размер списка
+print("список:", lst)
 
 for i in range(1, n-1):
     if lst[i] < lst[i-1] and lst[i] < lst[i+1]:
-        print(i)
+        print(f'локальный минимум: {i}')
         break
 else:
     print("Нет локального минимума")
--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,12 +1,17 @@
 #Дан список размера N (N — четное число). Поменять местами его первый элемент
 # со вторым, третий — с четвертым и т. д.
-n = int(input("N: "))
+import random
+
+n = random.randint(2, 10) * 2
 lst = []
 for i in range(n):
-    lst.append(int(input()))
+    lst.append(random.randint(1, 100))
+
+print("N =", n) # размер списка
+print("список:", lst)
 
 for i in range(0, n, 2):
     lst[i], lst[i+1] = lst[i+1], lst[i]
 
-print(lst)
+print("результат:", lst)
 
