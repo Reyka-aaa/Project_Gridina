07133be5f0ec94b7fe27102c2a70dce67b370951
rey <gridina.08@bk.ru>
2026-05-01 18:23:16 +0300

    8 практическая

---
+++ PZ_8/PZ_8_1.py
@@ -0,0 +1,5 @@
+#Создать словарь из списков keys = ['Ten', 'Twenty', 'Thirty'] и values = [10, 20, 30]
+k = ['Ten', 'Twenty', 'Thirty']
+v = [10, 20, 30]
+d = dict(zip(k, v))
+print(d)
--- RZ_8/PZ_8_1.py
+++
@@ -1,9 +0,0 @@
-#Создать словарь из списков keys = ['Ten', 'Twenty', 'Thirty'] и values = [10, 20, 30]
-keys = ['Ten', 'Twenty', 'Thirty']
-values = [10, 20, 30]
-
-result_dict = {}
-for i in range(len(keys)):
-    result_dict.update({keys[i]: values[i]})  # метод update()
-
-print(result_dict)
